51bd0eadc9b2df7d1bbf1b12824dbec8e28f20a0
DarylBM <145096328+DarylBM@users.noreply.github.com>
2023-09-27 10:30:54 +0800

    view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/daryl', 'Home::daryl');
+$routes->get('/product', 'ProductController::daryl');
+$routes->get('/product/(:any)', 'ProductController::product/$1');
--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,6 +6,14 @@ use App\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
+    public function product($product)
+    {
+    echo $product;
+    }
+    public function daryl()
+    {
+    return view('products');
+    }
     public function index()
     {
         //
